12dfa38b65507c082e82a3787d5720545d091b13
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2022-11-11 15:43:54 -0300

    .

--- src/Produto.js
+++ src/Produto.js
@@ -15,7 +15,7 @@ const Produto = ({ produto }) => {
     <div>
       <h1>{dados.nome}</h1>
       <p>R$ {dados.preco}</p>
-      <img src={dados.fotos[0].src}></img>
+      <img src={dados.fotos[0].src} alt="foto"></img>
     </div>
   );
 };
